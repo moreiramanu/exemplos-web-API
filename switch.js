cdf5f4f04778644b6287f66acb0d954e75b6ebe5
Ber <mauvemagician@gmail.com>
2025-07-17 16:34:10 -0300

    Primitivas e forms

---
+++ switch.js
@@ -0,0 +1,12 @@
+let a = 1;
+
+switch (a) {
+  case 1:
+    console.log("O valor de a é 1");
+    break;
+  case 2:
+    console.log("O valor de a é 2");
+    break;
+  default:
+    console.log("O valor de a não é nem 1 nem 2");
+}
