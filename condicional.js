432ad62896a5457da31086e958b66803fdb5f0b6
Ber <mauvemagician@gmail.com>
2025-07-17 15:57:45 -0300

    add condicionais

---
+++ condicional.js
@@ -0,0 +1,11 @@
+function comparar(x, y) {
+  if (x > y) {
+    console.log(x + " é maior");
+  } else if (y > x) {
+    console.log(y + " é maior");
+  } else {
+    console.log("Os dois valores são iguais");
+  }
+}
+
+comparar(100, 20);
